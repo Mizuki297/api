a3468ec4742754ad3bc24ce874d38cc6ea31d727
Mizuki297 <thanaphat.20297@gmail.com>
2019-08-30 14:30:27 +0700

    update request

--- get_post_request.php
+++ get_post_request.php
@@ -1,12 +1,21 @@
 <?php
 require 'ConnectDB.php';
-$sql = "select * from request;";
+$HPT_name = $_POST['HPT_name'];
+$Blood_type = $_POST['Blood_type'];
+
+$sql = "select * from data_cat where 
+HPT_name = '$HPT_name' 
+and Blood_type = '$Blood_type'
+and status_cat = 0;";
 $result = $conn->query($sql);
 
-if($result->num_rows >= 1){
-    while($row = $result->fetch_assoc()){
-        $output[] = $row;
-    }
-}echo json_encode($output);
+if($result->num_rows==1){
+    echo '1';
 
+}else{
+    echo '0';
+}
+$conn->close();
+
+?>
 ?>
