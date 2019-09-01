e7bc129956a04ca612f01784cca8369ab478885a
Mizuki297 <thanaphat.20297@gmail.com>
2019-09-02 00:40:15 +0700

    fix bug

--- get_post_request.php
+++ get_post_request.php
@@ -1,34 +1,16 @@
 <?php
 require 'ConnectDB.php';
 
-
 $HPT_name = $_POST['HPT_name'];
 $Blood_type = $_POST['Blood_type'];
 
 $user_id = $_POST['user_id'];
 
-//yyyy-mm-dd
-$date = date('y/m/d',time());
+$get_hpt_id = $conn->query("select HPT_id from hpt_pet where HPT_name = '$HPT_name';")
+->fetch_object()->HPT_id;
 
-$get_id = $conn->query("select max(request_id)+1 from request;");
-$next_id = $get_id->fetch_assoc();
-
-$get_hpt_id = $conn->query("select HPT_id from hpt_pet where HPT_name = '$HPT_name';");
-
-$sql = "insert into request (request_id, blood_type, date_request, HPT_id, user_id)
-values ('$next_id', '$Blood_type','$date' ,'$get_hpt_id', '$user_id');";
-
-// $sql = "select * from data_cat where 
-// Blood_type = 'O'
-// and status_cat = 0
-// and ";
-
-// $sql = "select * from data_cat where 
-// Blood_type = '$Blood_type'
-// and status_cat = 0
-// and HPT_id = (select HTP_id
-// from htp_pet where HPT_name = '$HPT_name' );";
-
+$sql = "insert into request (request_id ,blood_type, HPT_id, user_id)
+values (NULL,'$Blood_type', $get_hpt_id, $user_id);";
 
 if($result = $conn->query($sql)){
  
@@ -38,6 +20,7 @@ if($result = $conn->query($sql)){
     else{
     
     echo 'Try Again';
+    
     
     }
 $conn->close();
