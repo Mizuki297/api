742363cb2c8d31a56e49125be47a41ed60bab3bc
Mizuki297 <thanaphat.20297@gmail.com>
2019-09-03 09:29:40 +0700

    add php

--- show_data_cat.php
+++ show_data_cat.php
@@ -1,7 +1,11 @@
 <?php
 require 'ConnectDB.php';
-$sql = "select * from data_cat;";
-//เอา user ค้น id line
+
+$user_id = $_POST["user_id"];
+
+$sql = "select url_cat, user_id, blood_type, cat_type, cat_weight, 
+cat_bd, health_check_date, latest_donation from data_cat where user_id = '$user_id';";
+
 $result = $conn->query($sql);
 
 if($result->num_rows >= 1){
